5efa5da4dd9308ffc94b54de5a074359ba36091c
yaojl <jiali.yao@emc.com>
2012-10-26 02:58:11 -0700

    skip large datasets for performance test of kmeans rewrite

--- dataset/DataModeling/UnsupervisedLearning/kMeansClustering/kmeans_func.sql
+++ dataset/DataModeling/UnsupervisedLearning/kMeansClustering/kmeans_func.sql
@@ -41,51 +41,51 @@ AS $$
 $$;
 
 
-DROP TABLE IF EXISTS madlibtestdata.km_us_census_1990_rows_10_times;
-CREATE TABLE madlibtestdata.km_us_census_1990_rows_10_times(pid int8, position float8[]);
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+2458285, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+4916570, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+7374855, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+9833140, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+12291425, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+14749710, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+17207995, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+19666280, position FROM madlibtestdata.km_us_census_1990;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+22124565, position FROM madlibtestdata.km_us_census_1990;
+-- DROP TABLE IF EXISTS madlibtestdata.km_us_census_1990_rows_10_times;
+-- CREATE TABLE madlibtestdata.km_us_census_1990_rows_10_times(pid int8, position float8[]);
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+2458285, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+4916570, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+7374855, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+9833140, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+12291425, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+14749710, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+17207995, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+19666280, position FROM madlibtestdata.km_us_census_1990;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_10_times SELECT pid+22124565, position FROM madlibtestdata.km_us_census_1990;
 
-DROP TABLE IF EXISTS madlibtestdata.km_us_census_1990_rows_100_times;
-CREATE TABLE madlibtestdata.km_us_census_1990_rows_100_times(pid int8, position float8[]);
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+24582850, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+49165700, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+73748550, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+98331400, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+122914250, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+147497100, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+172079950, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+196662800, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
-INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+221245650, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- DROP TABLE IF EXISTS madlibtestdata.km_us_census_1990_rows_100_times;
+-- CREATE TABLE madlibtestdata.km_us_census_1990_rows_100_times(pid int8, position float8[]);
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+24582850, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+49165700, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+73748550, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+98331400, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+122914250, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+147497100, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+172079950, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+196662800, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
+-- INSERT INTO madlibtestdata.km_us_census_1990_rows_100_times SELECT pid+221245650, position FROM madlibtestdata.km_us_census_1990_rows_10_times;
 
-DROP TABLE IF EXISTS madlibtestdata.km_p53_columns_10_times;
-CREATE TABLE madlibtestdata.km_p53_columns_10_times AS SELECT pid, array_cat(array_cat(array_cat(array_cat(position, position),array_cat(position, position)),array_cat(array_cat(position, position),array_cat(position, position))),array_cat(position, position)) AS position FROM madlibtestdata.km_p53;
+-- DROP TABLE IF EXISTS madlibtestdata.km_p53_columns_10_times;
+-- CREATE TABLE madlibtestdata.km_p53_columns_10_times AS SELECT pid, array_cat(array_cat(array_cat(array_cat(position, position),array_cat(position, position)),array_cat(array_cat(position, position),array_cat(position, position))),array_cat(position, position)) AS position FROM madlibtestdata.km_p53;
 
-DROP TABLE IF EXISTS madlibtestdata.km_p53_columns_100_times;
-CREATE TABLE madlibtestdata.km_p53_columns_100_times AS SELECT pid, array_cat(array_cat(array_cat(array_cat(position, position),array_cat(position, position)),array_cat(array_cat(position, position),array_cat(position, position))),array_cat(position, position)) AS position FROM madlibtestdata.km_p53_columns_10_times;
+-- DROP TABLE IF EXISTS madlibtestdata.km_p53_columns_100_times;
+-- CREATE TABLE madlibtestdata.km_p53_columns_100_times AS SELECT pid, array_cat(array_cat(array_cat(array_cat(position, position),array_cat(position, position)),array_cat(array_cat(position, position),array_cat(position, position))),array_cat(position, position)) AS position FROM madlibtestdata.km_p53_columns_10_times;
 
-DROP TABLE IF EXISTS madlibtestdata.km_tfidf3_rows_2_times;
-CREATE TABLE madlibtestdata.km_tfidf3_rows_2_times(pid int8, position float8[]);
-INSERT INTO madlibtestdata.km_tfidf3_rows_2_times SELECT pid, position FROM madlibtestdata.km_tfidf3;
-INSERT INTO madlibtestdata.km_tfidf3_rows_2_times SELECT pid+703250, position FROM madlibtestdata.km_tfidf3;
+-- DROP TABLE IF EXISTS madlibtestdata.km_tfidf3_rows_2_times;
+-- CREATE TABLE madlibtestdata.km_tfidf3_rows_2_times(pid int8, position float8[]);
+-- INSERT INTO madlibtestdata.km_tfidf3_rows_2_times SELECT pid, position FROM madlibtestdata.km_tfidf3;
+-- INSERT INTO madlibtestdata.km_tfidf3_rows_2_times SELECT pid+703250, position FROM madlibtestdata.km_tfidf3;
 
-DROP TABLE IF EXISTS madlibtestdata.km_tfidf3_rows_4_times;
-CREATE TABLE madlibtestdata.km_tfidf3_rows_4_times(pid int8, position float8[]);
-INSERT INTO madlibtestdata.km_tfidf3_rows_4_times SELECT pid, position FROM madlibtestdata.km_tfidf3_rows_2_times;
-INSERT INTO madlibtestdata.km_tfidf3_rows_4_times SELECT pid+1406500, position FROM madlibtestdata.km_tfidf3_rows_2_times;
+-- DROP TABLE IF EXISTS madlibtestdata.km_tfidf3_rows_4_times;
+-- CREATE TABLE madlibtestdata.km_tfidf3_rows_4_times(pid int8, position float8[]);
+-- INSERT INTO madlibtestdata.km_tfidf3_rows_4_times SELECT pid, position FROM madlibtestdata.km_tfidf3_rows_2_times;
+-- INSERT INTO madlibtestdata.km_tfidf3_rows_4_times SELECT pid+1406500, position FROM madlibtestdata.km_tfidf3_rows_2_times;
 
-DROP TABLE IF EXISTS madlibtestdata.km_tfidf3_columns_2_times;
-CREATE TABLE madlibtestdata.km_tfidf3_columns_2_times AS SELECT pid, array_cat(position, position) AS position FROM madlibtestdata.km_tfidf3;
+-- DROP TABLE IF EXISTS madlibtestdata.km_tfidf3_columns_2_times;
+-- CREATE TABLE madlibtestdata.km_tfidf3_columns_2_times AS SELECT pid, array_cat(position, position) AS position FROM madlibtestdata.km_tfidf3;
 
-DROP TABLE IF EXISTS madlibtestdata.km_tfidf3_columns_4_times;
-CREATE TABLE madlibtestdata.km_tfidf3_columns_4_times AS SELECT pid, array_cat(position, position) AS position FROM madlibtestdata.km_tfidf3_columns_2_times;
+-- DROP TABLE IF EXISTS madlibtestdata.km_tfidf3_columns_4_times;
+-- CREATE TABLE madlibtestdata.km_tfidf3_columns_4_times AS SELECT pid, array_cat(position, position) AS position FROM madlibtestdata.km_tfidf3_columns_2_times;
 
