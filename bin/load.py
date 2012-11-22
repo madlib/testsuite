7c9d1cc4eecf54f60e351af528c5e892cb4b6d7b
yaojl <jiali.yao@emc.com>
2012-11-21 22:35:36 -0800

    Refine the load.py

--- bin/load.py
+++ bin/load.py
@@ -3,9 +3,14 @@ import sys
 SrcDir = '../src/'
 sys.path.append(SrcDir)
 from loader.loadingManager import loadingManager
+from  utility import analytics_tool, file_path
+
+Path = file_path.Path()
 
 def main():
-    loading_manager = loadingManager('..', 'madlibtestdata')
+    analyticsTools = analytics_tool.AnalyticsTools(Path.AnalyticsToolXml)
+    analyticsTools.parseTools()
+    loading_manager = loadingManager(Path.RootPath, 'madlibtestdata', analyticsTools)
     loading_manager.do(None, False, True, True)
 
 
